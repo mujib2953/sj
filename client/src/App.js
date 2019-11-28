3f988ebd2ae57f19f534135a90f5981c492ba696
Mujibur Rehman <mujibur.rehman@citiustech.com>
2019-11-28 23:43:54 +0530

    created mock jobs and displayed on UI.

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import Jobs from './components/Jobs';
 
+const mock_jobs = [
+	{title: "SWE 1", company: "Google"},
+	{title: "SWE 1", company: "Facebook"},
+	{title: "SWE 1", company: "Apple"},
+	{title: "SWE 1", company: "Amazon"},
+];
+
 function App() {
 	return (
 		<div className="App">
-			<Jobs />
+			<Jobs
+				jobs={ mock_jobs }
+			/>
 		</div>
 	);
 }
--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-export default function Job() {
+export default function Job({ job }) {
     return (
         <div className="job">
+            { job.title }
+            { job.company }
         </div>
     );
 }
