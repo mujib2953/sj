90d7fee63ebe4becf65837b177ecb9c26cd818bd
mujib2953 <ansarimujiburrehman@gmail.com>
2019-11-30 00:39:03 +0530

    WIP: storing jobs to redis server.

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,4 +1,10 @@
 const fetch = require("node-fetch");
+const redis = require("redis");
+const { promisify } = require("util");
+
+const client = redis.createClient();
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 const BASE_URL = "https://jobs.github.com/positions.json";
 
@@ -20,4 +26,6 @@ module.exports =  async function fetchGithub() {
         console.log(`Found ${resultCount} jobs.`);
     }
     console.log(`Total jobs ${allJobs.length} found`);
+
+    const success = await setAsync("github", JSON.stringify(allJobs));
 }
